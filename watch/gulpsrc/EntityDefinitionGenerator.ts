b244bcd277aa25afa2f505c28fd63e997dc936d0
russoturisto <russoturisto@gmail.com>
2016-03-28 01:05:24 -0600

    Added generator
    The core generator

---
+++ watch/gulpsrc/EntityDefinitionGenerator.ts
@@ -0,0 +1,133 @@
+/// <reference path="typings/main.d.ts" />
+
+/**
+ * Created by Papa on 3/26/2016.
+ */
+import * as ts from "typescript";
+import * as fs from 'fs';
+import SyntaxKind = ts.SyntaxKind;
+import {EntityCandidate} from "./parser/EntityCandidate";
+import {EntityCandidateRegistry} from "./parser/EntityCandidateRegistry";
+
+interface DocEntry {
+	name?:string,
+	fileName?:string,
+	documentation?:string,
+	type?:string,
+	constructors?:DocEntry[],
+	parameters?:DocEntry[],
+	properties?:DocEntry[],
+	returnType?:string
+}
+
+var rootEntity = new EntityCandidate('Entity', null, 'PHibernate', true);
+var entityCandidateRegistry = new EntityCandidateRegistry(rootEntity);
+
+/** Generate documention for all classes in a set of .ts files */
+function generateEntityDefinitions(
+	fileNames:string[],
+	options:ts.CompilerOptions
+):void {
+	// Build a program using the set of root file names in fileNames
+	let program = ts.createProgram(fileNames, options);
+
+	// Get the checker, we will use it to find more about classes
+	let checker = program.getTypeChecker();
+
+	let output:DocEntry[] = [];
+
+	// Visit every sourceFile in the program
+	for (const sourceFile of program.getSourceFiles()) {
+		// Walk the tree to search for classes
+		ts.forEachChild(sourceFile, visit);
+	}
+
+	// print out the doc
+	fs.writeFileSync("classes.json", JSON.stringify(output, undefined, 4));
+
+	return;
+
+	/** visit nodes finding exported classes */
+	function visit( node:ts.Node ) {
+		// Only consider exported nodes
+		if (!isNodeExported(node)) {
+			return;
+		}
+
+		if (node.kind === ts.SyntaxKind.ClassDeclaration) {
+			// This is a top level class, get its symbol
+			let symbol = checker.getSymbolAtLocation((<ts.ClassDeclaration>node).name);
+			let serializedClass = serializeClass(symbol);
+			if (serializedClass) {
+				output.push(serializedClass);
+			}
+			// No need to walk any further, class expressions/inner declarations
+			// cannot be exported
+		}
+		else if (node.kind === ts.SyntaxKind.ModuleDeclaration) {
+			// This is a namespace, visit its children
+			ts.forEachChild(node, visit);
+		}
+	}
+
+
+	/** Serialize a symbol into a json object */
+	function serializeSymbol( symbol:ts.Symbol ):DocEntry {
+		return {
+			name: symbol.getName(),
+			documentation: ts.displayPartsToString(symbol.getDocumentationComment()),
+			type: checker.typeToString(checker.getTypeOfSymbolAtLocation(symbol, symbol.valueDeclaration))
+		};
+	}
+
+	/** Serialize a class symbol information */
+	function serializeClass( symbol:ts.Symbol ) {
+		let details = serializeSymbol(symbol);
+
+		if (!importsEntity(<ts.Node><any>symbol)) {
+			return null;
+		}
+
+		if (!extendsEntity(symbol)) {
+			return null;
+		}
+
+		let properties:DocEntry[] = [];
+
+		for (let memberName in symbol.members) {
+			let member = symbol.members[memberName];
+			if (member.valueDeclaration.kind === SyntaxKind.PropertyDeclaration) {
+				console.log(`Property: ${memberName}`);
+				let propertySymbolDescriptor = serializeSymbol(member);
+				properties.push(propertySymbolDescriptor);
+				console.log(propertySymbolDescriptor);
+			}
+		}
+		details.properties = properties;
+
+		// Get the construct signatures
+		let constructorType = checker.getTypeOfSymbolAtLocation(symbol, symbol.valueDeclaration);
+		details.constructors = constructorType.getConstructSignatures().map(serializeSignature);
+		return details;
+	}
+
+
+
+	/** Serialize a signature (call or construct) */
+	function serializeSignature( signature:ts.Signature ) {
+		return {
+			parameters: signature.parameters.map(serializeSymbol),
+			returnType: checker.typeToString(signature.getReturnType()),
+			documentation: ts.displayPartsToString(signature.getDocumentationComment())
+		};
+	}
+
+	/** True if this is visible outside this file, false otherwise */
+	function isNodeExported( node:ts.Node ):boolean {
+		return (node.flags & ts.NodeFlags.Export) !== 0 || (node.parent && node.parent.kind === ts.SyntaxKind.SourceFile);
+	}
+}
+
+generateEntityDefinitions(process.argv.slice(2), {
+	target: ts.ScriptTarget.ES5, module: ts.ModuleKind.CommonJS
+});
