3cb93b10503566c227bf89237828dab00ce7565b
russoturisto <russoturisto@gmail.com>
2016-04-30 16:31:09 -0600

    missed compilation

--- lib/core/metadata/decorators.d.ts
+++ lib/core/metadata/decorators.d.ts
@@ -1,12 +1,39 @@
 /**
  * Created by Papa on 4/17/2016.
  */
+/**
+ * Annotates Id fields of Entities.
+ *
+ * @returns {function(any, string)}
+ * @constructor
+ */
 export declare function Id(): (targetObject: any, propertyKey: string) => void;
 export interface EntityConfiguration {
 }
+/**
+ * Annotates entities.
+ *
+ * @param entityConfiguration
+ * @returns {function(Function)}
+ * @constructor
+ */
 export declare function Entity(entityConfiguration?: EntityConfiguration): (constructor: Function) => void;
+/**
+ * Annotates collections of Entities in other entities.
+ *
+ * @param foreignKeyFieldName
+ * @returns {function(any, string)}
+ * @constructor
+ */
 export declare function ForeignKey(foreignKeyFieldName: string): (targetObject: any, propertyKey: string) => void;
 export interface RepositoryConfiguration {
 }
 export declare function Repository(repositoryConfiguration?: RepositoryConfiguration): (constructor: Function) => void;
-export declare function Query(queryDefinition: string): (targetObject: any, propertyKey: string, descriptor: PropertyDescriptor) => void;
+/**
+ * Annotates query function pointers (variables).
+ *
+ * @param queryDefinition
+ * @returns {function(any, string)}
+ * @constructor
+ */
+export declare function Query(queryDefinition: string): (targetObject: any, propertyKey: string) => void;
