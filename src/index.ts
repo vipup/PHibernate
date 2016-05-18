c8e145105728fff4058562d9d90e4ae911c8dd61
russoturisto <russoturisto@gmail.com>
2016-05-18 06:15:37 -0600

    publish changes

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,23 @@
 import {Entity, EntityConfiguration, ForeignKey, Id, Query, Repository, RepositoryConfiguration} from "./core/metadata/decorators";
 import {QueryState} from "./core/query/QueryState";
 import {IRepository, QRepository} from "./core/repository/Repository";
+import {FieldType, IEntityProxyWrapper, IProxiedEntity, IProxiedEntityClass} from "./core/proxy/Proxies";
+import {ProxyGenerator} from "./core/proxy/ProxyGenerator";
 
 export {
 	Entity,
 	EntityConfiguration,
+	FieldType,
 	ForeignKey,
 	Id,
+	IEntityProxyWrapper,
+	IProxiedEntity,
+	IProxiedEntityClass,
 	IRepository,
 	QRepository,
 	Query,
 	QueryState,
+	ProxyGenerator,
 	Repository,
 	RepositoryConfiguration,
 };
