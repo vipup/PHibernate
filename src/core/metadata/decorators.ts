07358ca48b3576663242d6ab53956cd1132b45ff
russoturisto <russoturisto@gmail.com>
2016-07-27 18:57:11 -0600

    More Annotations

--- src/core/metadata/decorators.ts
+++ src/core/metadata/decorators.ts
@@ -3,13 +3,13 @@
  */
 
 import {
-	IQEntity, IQRelation, QEntity, QRelation,
-	RelationType, IEntity, QStringField, QNumberField, QDateField, IQStringField,
-	IQNumberField, IQDateField, IDateOperation, IStringOperation, INumberOperation, JSONStringOperation,
-	JSONNumberOperation, JSONDateOperation
+    IQEntity, IQRelation, QEntity, QRelation,
+    RelationType, IEntity, QStringField, QNumberField, QDateField, IQStringField,
+    IQNumberField, IQDateField, IDateOperation, IStringOperation, INumberOperation, JSONStringOperation,
+    JSONNumberOperation, JSONDateOperation
 } from "querydsl-typescript/lib/index";
 import {PH} from "../../config/PH";
-import {ManyToOneElements, OneToManyElements} from "../../config/JPAApi";
+import {AccessType, GenerationType, ManyToOneElements, OneToManyElements} from "../../config/JPAApi";
 
 export const PH_PRIMARY_KEY = '__primaryKey__';
 export const PH_MANY_TO_ONE = '__ph_many_to_one__';
@@ -25,15 +25,13 @@ export const PH_EDITABLE_FOR = '__ph_editable_for__';
  * @constructor
  */
 export function Id() {
-	return function (
-		targetObject:any,
-		propertyKey:string
-	) {
-		if(targetObject[PH_PRIMARY_KEY]) {
-			throw `Cannot set primary key to '${propertyKey}', it is already set to '${targetObject[PH_PRIMARY_KEY]}'`;
-		}
-		targetObject[PH_PRIMARY_KEY] = propertyKey;
-	}
+    return function (targetObject:any,
+                     propertyKey:string) {
+        if (targetObject[PH_PRIMARY_KEY]) {
+            throw `Cannot set primary key to '${propertyKey}', it is already set to '${targetObject[PH_PRIMARY_KEY]}'`;
+        }
+        targetObject[PH_PRIMARY_KEY] = propertyKey;
+    }
 }
 
 export interface EntityConfiguration {
@@ -47,13 +45,135 @@ export interface EntityConfiguration {
  * @returns {function(Function)}
  * @constructor
  */
-export function Entity(
-	entityConfiguration?:EntityConfiguration
-) {
-	return function (
-		constructor:Function
-	) {
-	}
+export function Entity(entityConfiguration?:EntityConfiguration) {
+    return function (constructor:Function) {
+    }
+}
+
+export interface TableConfiguration {
+    name:string;
+}
+
+/**
+ * Annotates tables.
+ *
+ * @param tableConfiguration
+ * @returns {function(Function)}
+ * @constructor
+ */
+export function Table(tableConfiguration?:TableConfiguration) {
+    return function (constructor:Function) {
+    }
+}
+
+/**
+ * Annotates tables.
+ *
+ * @param tableConfiguration
+ * @returns {function(Function)}
+ * @constructor
+ */
+export function MappedSuperclass() {
+    return function (constructor:Function) {
+    }
+}
+
+export interface SequenceGeneratorConfiguration {
+    name:string;
+    sequenceName:string;
+}
+
+/**
+ * Annotates tables.
+ *
+ * @param tableConfiguration
+ * @returns {function(Function)}
+ * @constructor
+ */
+export function SequenceGenerator(sequenceGeneratorConfiguration?:SequenceGeneratorConfiguration) {
+    return function (constructor:Function) {
+    }
+}
+
+export interface ColumnConfiguration {
+    name:string;
+}
+
+/**
+ * Annotates columns.
+ *
+ * @param columnConfiguration
+ * @returns {function(Function)}
+ * @constructor
+ */
+export function Column(columnConfiguration?:ColumnConfiguration) {
+    return function (targetObject:any,
+                     propertyKey:string) {
+    }
+}
+
+export interface GeneratedValueConfiguration {
+    generator:string;
+    strategy:GenerationType;
+}
+
+/**
+ * Annotates columns.
+ *
+ * @param generatedValueConfiguration
+ * @returns {function(Function)}
+ * @constructor
+ */
+export function GeneratedValue(generatedValueConfiguration?:GeneratedValueConfiguration) {
+    return function (targetObject:any,
+                     propertyKey:string) {
+    }
+}
+
+
+/**
+ * Annotates columns.
+ *
+ * @param generatedValueConfiguration
+ * @returns {function(Function)}
+ * @constructor
+ */
+export function Access(accessType:AccessType) {
+    return function (targetObject:any,
+                     propertyKey:string) {
+    }
+}
+
+
+export interface JoinColumnConfiguration {
+    name:string;
+    nullable:boolean;
+}
+
+/**
+ * Annotates columns.
+ *
+ * @param columnConfiguration
+ * @returns {function(Function)}
+ * @constructor
+ */
+export function JoinColumn(joinColumnConfiguration?:JoinColumnConfiguration) {
+    return function (targetObject:any,
+                     propertyKey:string) {
+    }
+}
+
+/**
+ * Annotates columns.
+ *
+ * @param generatedValueConfiguration
+ * @returns {function(Function)}
+ * @constructor
+ */
+export function Transient() {
+    return function (targetObject:any,
+                     propertyKey:string) {
+    }
 }
 
 /**
@@ -65,20 +185,16 @@ export function Entity(
  * @returns {function(any, string)}
  * @constructor
  */
-export function ManyToOne(
-	elements?:ManyToOneElements
-) {
-	return function (
-		targetObject:any,
-		propertyKey:string
-	) {
-		let manyToOne = targetObject[PH_MANY_TO_ONE];
-		if(!manyToOne) {
-			manyToOne = {};
-			targetObject[PH_MANY_TO_ONE] = manyToOne;
-		}
-		manyToOne[propertyKey] = elements;
-	}
+export function ManyToOne(elements?:ManyToOneElements) {
+    return function (targetObject:any,
+                     propertyKey:string) {
+        let manyToOne = targetObject[PH_MANY_TO_ONE];
+        if (!manyToOne) {
+            manyToOne = {};
+            targetObject[PH_MANY_TO_ONE] = manyToOne;
+        }
+        manyToOne[propertyKey] = elements;
+    }
 
 }
 
@@ -91,46 +207,38 @@ export function ManyToOne(
  * @returns {function(any, string)}
  * @constructor
  */
-export function OneToMany(
-	elements?:OneToManyElements
-) {
-	return function (
-		targetObject:any,
-		propertyKey:string
-	) {
-		let oneToMany = targetObject[PH_ONE_TO_MANY];
-		if(!oneToMany) {
-			oneToMany = {};
-			targetObject[PH_ONE_TO_MANY] = oneToMany;
-		}
-		oneToMany[propertyKey] = elements;
-	}
+export function OneToMany(elements?:OneToManyElements) {
+    return function (targetObject:any,
+                     propertyKey:string) {
+        let oneToMany = targetObject[PH_ONE_TO_MANY];
+        if (!oneToMany) {
+            oneToMany = {};
+            targetObject[PH_ONE_TO_MANY] = oneToMany;
+        }
+        oneToMany[propertyKey] = elements;
+    }
 
 }
 
 export interface AccessControlElements {
-	roles: string[]
+    roles:string[]
 }
 
 export interface VisibleForElements extends AccessControlElements {
 
 }
 
-export function VisibleFor(
-	elements:VisibleForElements
-) {
+export function VisibleFor(elements:VisibleForElements) {
 
-	return function (
-		targetObject:any,
-		propertyKey:string
-	) {
-		let visibleForMap = targetObject[PH_VISIBLE_FOR];
-		if(!visibleForMap) {
-			visibleForMap = {};
-			targetObject[PH_VISIBLE_FOR] = visibleForMap;
-		}
-		visibleForMap[propertyKey] = elements;
-	}
+    return function (targetObject:any,
+                     propertyKey:string) {
+        let visibleForMap = targetObject[PH_VISIBLE_FOR];
+        if (!visibleForMap) {
+            visibleForMap = {};
+            targetObject[PH_VISIBLE_FOR] = visibleForMap;
+        }
+        visibleForMap[propertyKey] = elements;
+    }
 
 }
 
@@ -138,21 +246,17 @@ export interface EditableForElements extends AccessControlElements {
 
 }
 
-export function EditableFor(
-	elements:EditableForElements
-) {
+export function EditableFor(elements:EditableForElements) {
 
-	return function (
-		targetObject:any,
-		propertyKey:string
-	) {
-		let editableForMap = targetObject[PH_EDITABLE_FOR];
-		if(!editableForMap) {
-			editableForMap = {};
-			targetObject[PH_EDITABLE_FOR] = editableForMap;
-		}
-		editableForMap[propertyKey] = elements;
-	}
+    return function (targetObject:any,
+                     propertyKey:string) {
+        let editableForMap = targetObject[PH_EDITABLE_FOR];
+        if (!editableForMap) {
+            editableForMap = {};
+            targetObject[PH_EDITABLE_FOR] = editableForMap;
+        }
+        editableForMap[propertyKey] = elements;
+    }
 
 }
 
@@ -160,12 +264,10 @@ export interface RepositoryConfiguration {
 
 }
 
-export function Repository(
-	repositoryConfiguration?:RepositoryConfiguration
-) {
+export function Repository(repositoryConfiguration?:RepositoryConfiguration) {
 
-	return function (constructor:Function) {
-	}
+    return function (constructor:Function) {
+    }
 
 }
 
@@ -177,22 +279,20 @@ export function Repository(
  * @returns {function(any, string)}
  * @constructor
  */
-export function Query<IE extends IEntity, IParams>(
-	entityClass:any,
-	selectDefinition:IE | {(select:any):IE},
-	fromWhereDefinition:IE | {(q:PH, params:IParams):IE},
-  paramsFactory:{():IParams}
-) {
+export function Query<IE extends IEntity, IParams>(entityClass:any,
+                                                   selectDefinition:IE | {(select:any):IE},
+                                                   fromWhereDefinition:IE | {(q:PH, params:IParams):IE},
+                                                   paramsFactory:{():IParams}) {
 
-	return function (target, propertyKey:string) {
-		Object.defineProperty(target, propertyKey, {
-			get: function () {
-				throw `Not implemented yet`;
-			},
-			set: function (val) {
-			throw `Cannot override '${propertyKey}' @Query annotated reference, please define a parameter function.`;
-			}
-		});
-	}
+    return function (target, propertyKey:string) {
+        Object.defineProperty(target, propertyKey, {
+            get: function () {
+                throw `Not implemented yet`;
+            },
+            set: function (val) {
+                throw `Cannot override '${propertyKey}' @Query annotated reference, please define a parameter function.`;
+            }
+        });
+    }
 
 }
