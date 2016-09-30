47fdd8daa3a58dfe87140b6350edcedc1192e3d0
russoturisto <russoturisto@gmail.com>
2016-09-30 07:12:05 -0600

    started remote change application code

--- src/changeList/offline/sql/OfflineSqlDeltaStore.ts
+++ src/changeList/offline/sql/OfflineSqlDeltaStore.ts
@@ -2,7 +2,7 @@ import {IOfflineDeltaStore} from "../../OfflineDeltaStore";
 import {ILocalStoreAdaptor} from "../../../localStore/LocalStoreAdaptor";
 import {StubChangeGroup, ChangeGroupApi} from "../../model/ChangeGroup";
 import {IOfflineDeltaStoreConfig} from "../../../config/OfflineDeltaStoreConfig";
-import {IEntityChange} from "../../model/EntityChange";
+import {IEntityChange, EntityChange} from "../../model/EntityChange";
 import {QChangeGroup} from "../../query/changegroup";
 import {and, or} from "querydsl-typescript";
 import {QEntityChange} from "../../query/entitychange";
@@ -19,27 +19,29 @@ export class OfflineSqlDeltaStore implements IOfflineDeltaStore {
 	}
 
 	async addRemoteChanges(
-		changeRecords: ChangeGroupApi[]
+		changeGroups: ChangeGroupApi[]
 	): Promise<ChangeGroupApi[]> {
 		let entityIdMap: {[entityId: string]: boolean} = {};
-		let earliestDate = changeRecords.map((changeRecord) => {
-			changeRecord.entityChanges.forEach((entityChange) => {
+		let earliestDate = changeGroups.map((changeGroup) => {
+			changeGroup.entityChanges.sort(this.sortEntityChanges);
+			changeGroup.entityChanges.forEach((entityChange) => {
 				entityChange.changedEntityId;
 				entityIdMap[entityChange.changedEntityId] = true;
 			});
-			return changeRecord.createDateTime;
+			return changeGroup.createDateTime;
 		}).reduce((previousDate, currentDate) => {
 			if (previousDate.getTime() > currentDate.getTime()) {
 				return currentDate;
 			}
 			return previousDate;
 		});
-		let entityIds:string[] = [];
-		for(let entityId in entityIdMap) {
+		let entityIds: string[] = [];
+		for (let entityId in entityIdMap) {
 			entityIds.push(entityId);
 		}
-		let cg:, ec;
-		let localChangeRecords = await QChangeGroup.find({
+		// Find all local records for these entities since the time of the first incoming change
+		let cg, ec;
+		let localChangeGroups = await QChangeGroup.find({
 			select: {
 				id: null
 			},
@@ -52,7 +54,84 @@ export class OfflineSqlDeltaStore implements IOfflineDeltaStore {
 				ec.changedEntityId.isIn(entityIds)
 			)
 		});
-		return null;
+		changeGroups.sort(this.sortChangeGroups);
+		localChangeGroups.sort(this.sortChangeGroups);
+
+		if (localChangeGroups.length) {
+			this.filterOutOverwrittenChanges(changeGroups, localChangeGroups)
+		}
+
+		return changeGroups;
+	}
+
+	/**
+	 * Remove any modifications that were later updated by another change
+	 * @param changeGroups  remotely created change groups
+	 * @param localChangeGroups locally created change groups
+	 */
+	private filterOutOverwrittenChanges(
+		changeGroups: ChangeGroupApi[],
+		localChangeGroups: ChangeGroupApi[]
+	) {
+
+		let currLocalCGIndex = 0;
+		changeGroups.forEach((changeGroup) => {
+			while (currLocalCGIndex < localChangeGroups.length) {
+				let localChangeGroup = localChangeGroups[currLocalCGIndex];
+
+				if(this.sortChangeGroups(changeGroup, localChangeGroup) > 0) {
+					break;
+				}
+			}
+		});
+	}
+
+	private sortChangeGroups(cg1: ChangeGroupApi, cg2: ChangeGroupApi): number {
+		let time1 = cg1.createDateTime.getTime();
+		let time2 = cg2.createDateTime.getTime();
+		if (time1 > time2) {
+			return 1;
+		}
+		if (time2 > time1) {
+			return -1;
+		}
+		let deviceId1 = cg1.createDeviceId;
+		let deviceId2 = cg2.createDeviceId;
+		if (deviceId1 > deviceId2) {
+			return 1;
+		}
+		if (deviceId2 > deviceId1) {
+			return -1;
+		}
+		let userId1 = cg1.createUserId;
+		let userId2 = cg2.createUserId;
+		if (userId1 > userId2) {
+			return 1;
+		}
+		if (userId2 > userId1) {
+			return -1;
+		}
+		let inMilliIdx1 = cg1.groupIndexInMillisecond;
+		let inMilliIdx2 = cg2.groupIndexInMillisecond;
+		if (inMilliIdx1 > inMilliIdx2) {
+			return 1;
+		}
+		if (inMilliIdx2 > inMilliIdx1) {
+			return -1;
+		}
+		return 0;
+	}
+
+	private sortEntityChanges(ec1: EntityChange, ec2: EntityChange): number {
+		let id1 = ec1.entityChangeIdInGroup;
+		let id2 = ec2.entityChangeIdInGroup;
+		if (id1 > id2) {
+			return 1;
+		}
+		if (id2 > id1) {
+			return -1;
+		}
+		return 0;
 	}
 
 	async addChange(
